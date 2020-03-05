5ef7f1530f7ecc9c5db4014e18ca6d0d94e5534f
de_Mora <miguel.uni.demora@gmail.com>
2020-03-05 13:13:38 +0100

    heroku deploy changes

---
+++ server/public/precache-manifest.d4024413f2920a8b886ed5cbdac7eadf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84b240c2c286a93f1624e05049ed3a64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f646494f8bebb712d071",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1b7d2e340b049c8afc79",
+    "url": "/static/css/main.e8f2cc11.chunk.css"
+  },
+  {
+    "revision": "f646494f8bebb712d071",
+    "url": "/static/js/2.c3e66930.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.c3e66930.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b7d2e340b049c8afc79",
+    "url": "/static/js/main.ad589334.chunk.js"
+  },
+  {
+    "revision": "b777e1e5607df564d26b",
+    "url": "/static/js/runtime-main.aaf88d20.js"
+  }
+]);
